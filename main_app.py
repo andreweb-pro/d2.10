92ccaf304c97a7256db642cb40d8bdf889c38c73
andreweb-pro <67388610+andreweb-pro@users.noreply.github.com>
2021-02-01 17:50:30 +0500

    Update

--- main_app.py
+++ main_app.py
@@ -7,6 +7,27 @@ sentry_sdk.init(dsn=os.environ.get("SENTRY_DSN"), integrations=[BottleIntegratio
 
 app = Bottle()
 
+@app.route('/')
+def index():
+    form = """
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>D2</title>
+    </head>
+    <body>
+        <h1>This is main page</h1>
+        <hr>
+        <h2>For visit successful page, append to the end of URL "https://d2-10.herokuapp.com/success"</h2>
+        <hr>
+        <h2>For visit fail page, append to the end of URL "https://d2-10.herokuapp.com/fail"</h2>
+        <hr>
+    </body>
+</html>
+"""
+    return form
 
 @app.route('/success')
 def success():
